2df022a99ae5d54c17cef588b9223d2becf02b6b
tuancopy <tuanphamvu90@gmail.com>
2015-05-08 10:56:10 +0700

    update Behaviour Sluggable

---
+++ app/Config/Migration/1430988318_testmigration.php
@@ -0,0 +1,78 @@
+<?php
+class TestMigration extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'TestMigration';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'posts' => array(
+					'slug' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'after' => 'created'),
+				),
+			),
+			/*'alter_field' => array(
+				'posts' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
+				),
+			),*/
+			/*'create_table' => array(
+				'tuts' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
+					'username' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'password' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'role' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 20, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+					'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+					'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+					'group_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB'),
+				),
+			),*/
+		),
+		'down' => array(
+			/*'drop_field' => array(
+				'posts' => array('slug'),
+			),*/
+			/*'alter_field' => array(
+				'posts' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+				),
+			),*/
+			'drop_table' => array(
+				'tuts'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
